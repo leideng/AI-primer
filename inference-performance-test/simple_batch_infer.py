ea561861bcc99f055313631f575991ff07c519ce
ldeng <ldeng.sjtu@gmail.com>
2025-06-09 21:56:33 +0800

    add infer-perf-test category

---
+++ inference-performance-test/simple_batch_infer.py
@@ -0,0 +1,38 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import time
+
+from transformers import modeling_utils
+if not hasattr(modeling_utils, "ALL_PARALLEL_STYLES") or modeling_utils.ALL_PARALLEL_STYLES is None:
+    modeling_utils.ALL_PARALLEL_STYLES = ["tp", "none","colwise",'rowwise']
+
+model_name = "Qwen/Qwen3-8B"
+
+# load the tokenizer and the model
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+
+# prepare the model input
+prompt = "Give me a short introduction to large language model.keep it very short, 1 line only"
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True,
+    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+# conduct text completion
+start = time.time()
+for x in range(10):
+  generated_ids = model.generate(
+      **model_inputs,
+      max_new_tokens=100
+  )
+end = time.time()
